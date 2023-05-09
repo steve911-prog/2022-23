7912189db40f5a716508aaea3c984c3f944afab3
steve911-prog <bwlad0911@gmail.com>
2023-05-09 18:43:24 +0200

    Update

--- js/tasks.js
+++ js/tasks.js
@@ -1,8 +1,18 @@
-function createPhoneNumber(n){
+// function createPhoneNumber(n){
 
-  console.log(`(${n[0], n[1], n[2]}) ${n[3], n[4], n[5]}-${n[6], n[7], n[8]}`);
+//   console.log(`(${n[0], n[1], n[2]}) ${n[3], n[4], n[5]}-${n[6], n[7], n[8]}`);
 
-}
+// }
 
-createPhoneNumber([1,2,3,4,5,6,7,8,9,0])
+// createPhoneNumber([1,2,3,4,5,6,7,8,9,0])
+
+console.log("Hello,World!");
+
+let num = 5;
+
+/*klnkll
+mmmmlmk
+*/
+
+// 6:02
 
