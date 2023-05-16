6f4b72e222c72fbcf35f1f7aeeb1e7eebfa84f9a
steve911-prog <bwlad0911@gmail.com>
2023-05-16 22:34:56 +0200

    Leak fix

--- Weather in moment/main.py
+++ Weather in moment/main.py
@@ -1,8 +1,10 @@
 import requests
+import json
 
 CITY = "kharkiv"
-API = "49c620ef69a3645ac3037ba4962a366e"
+API = "2e500d85593210eabe2dd574e53fc677"
 
 response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API}&units=metric")
+data = json.loads(response.text)
 
-print(response.text)
+print(data)
