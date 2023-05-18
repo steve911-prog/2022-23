a407317c31fc6ae5d4b08caf06e86cba25411e0f
steve911-prog <bwlad0911@gmail.com>
2023-05-18 11:57:28 +0200

    Weather App Form

--- Weather in moment/main.py
+++ Weather in moment/main.py
@@ -1,13 +1,16 @@
-import requests
+import requests  #API
 import json
 
 CITY = "kharkiv"
 API = "2e500d85593210eabe2dd574e53fc677"
 
+#get request to openweather
 response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API}&units=metric")
+
+
 data = json.loads(response.text)
 
-print(data)
+# print(data)
 
 temp= data["main"]["temp"]
 feels_like= data["main"]["feels_like"]
@@ -16,3 +19,5 @@ wind_speed= data["wind"]["speed"]
 clouds= data["clouds"]
 description = data["weather"][0]["description"]
 icon= data["weather"][0]["icon"]
+
+print(f"Temperature: {temp}\n{description}\nWind speed: {wind_speed}\nClouds: {clouds}")
